c9e1f8501bbe22b52bfb29affefe87a226b0f370
Karan  Warrier <karan.warrier@gmail.com>
2025-05-04 00:17:50 -0400

    add new anthropic file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # kw-ai-agent-1
+
+
+if using python3, be sure to install requirements.txt with "pip3 install -r requirements.txt"
--- with_langchain.py
+++ with_langchain.py
@@ -10,6 +10,6 @@ from langchain_community.tools import DuckDuckGoSearchRun
 # Load environment variables (create a .env file with your API keys)
 load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = "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"
+# os.environ["OPENAI_API_KEY"] = "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"
 llm = ChatOpenAI()
 llm.invoke("Hello, world!")
